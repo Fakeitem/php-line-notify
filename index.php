5a7fc854439d1e3557b678beab3b04ac63bc6468
Fakeitem <non_scoobydoo@hotmail.com>
2018-10-12 02:33:13 +0700

    Remove junk files

--- index.php
+++ index.php
@@ -4,12 +4,10 @@
         <title>LINE Notify</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" type="image/png" href="css/images/myicon.png" />
+        <link rel="icon" type="image/png" href="" />
         <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
-        <link rel="stylesheet" type="text/css" href="css/app.css" />
         <script type="text/javascript" src="js/jquery.min.js"></script>
         <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
-        <script type="text/javascript" src="js/app.js"></script>
     </head>
     <body>
         <div class="container" style="padding-top: 15px; padding-bottom: 15px;">
